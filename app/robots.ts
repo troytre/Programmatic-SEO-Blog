22b9bbf5e4b76aceddd79d0264b1413403e0811c
troytre <64361506+troytre@users.noreply.github.com>
2026-01-16 21:18:14 +0530

    Update robots.ts
    Updated Robots with domain name

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,12 @@ import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
-    sitemap: "https://yourdomain.com/sitemap.xml",
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
+    sitemap: "https://programmatic-seo-blog.vercel.app/sitemap.xml",
   };
 }
